e62c30c7e02c786f99c95f6da6f6e4f0dc5b91e2
pateljiyaa <jiya92139@gmail.com>
2026-02-17 13:02:23 +0530

    Add files via upload

---
+++ P1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+      printf("\n arithmetic operation");
+      int a;
+      int b;
+      printf("\n enter value of a and b");
+      scanf("%d %d", &a,&b);
+      printf("\n addition=%d", a+b);
+      printf("\n sub=%d", a-b);
+      printf("\n mul=%d", a*b);
+      printf("\n div=%d", a/b);
+      printf("\n remainder=%d", a%b);
+      return 0;
+}
